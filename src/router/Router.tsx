a30d0e9d61cc9efda53d7cc4db9480f34da4ece4
pectobacterium <51320938+pectobacterium@users.noreply.github.com>
2022-08-11 04:37:45 +0900

    Initial commit

---
+++ src/router/Router.tsx
@@ -0,0 +1,46 @@
+import { memo, FC } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "../components/pages/Login";
+import { Home } from "../components/pages/Home";
+import { UserManagement } from "../components/pages/UserManagement";
+import { Setting } from "../components/pages/Setting";
+import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
+import { LoginUserProvider } from "../providers/LoginUserProvider";
+
+export const Router: FC = memo(() => {
+  return (
+    <LoginUserProvider>
+      <Routes>
+        <Route index path="/" element={<Login />} />
+        <Route path="/home">
+          <Route
+            index={true}
+            element={
+              <HeaderLayout>
+                <Home />
+              </HeaderLayout>
+            }
+          />
+          <Route
+            path="user_management"
+            element={
+              <HeaderLayout>
+                <UserManagement />{" "}
+              </HeaderLayout>
+            }
+          />
+          <Route
+            path="setting"
+            element={
+              <HeaderLayout>
+                <Setting />{" "}
+              </HeaderLayout>
+            }
+          />
+        </Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </LoginUserProvider>
+  );
+});
